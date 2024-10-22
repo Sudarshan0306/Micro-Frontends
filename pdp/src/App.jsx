c329c7d0991f6c103ab03892fc9f5f85a78f9758
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-10-22 12:14:06 +0530

    first commit

---
+++ pdp/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "home/index.scss";
+
+import Header from "home/Header";
+import Footer from "home/Footer";
+
+const App = () => (
+  <div className="text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div className="my-10"> PDP page content</div>
+    <Footer />
+  </div>
+);
+const rootElement = document.getElementById("app")
+if (!rootElement) throw new Error("Failed to find the root element")
+
+const root = ReactDOM.createRoot(rootElement)
+
+root.render(<App />)
